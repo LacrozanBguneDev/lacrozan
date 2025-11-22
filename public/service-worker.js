d6d7bc2360c5cb5330d0a70bf57f7ea0e2e21f0d
LacrozanBguneDev <lacrozanbgune@gmail.com>
2025-11-22 07:50:46 +0700

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,28 +1,29 @@
-// sw.js - Service Worker Sederhana
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
+importScripts('https://www.gstatic.com/firebasejs/11.6.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/11.6.1/firebase-messaging-compat.js');
+
+// Masukkan Config Firebase KAMU di sini (Sama seperti di file React)
+firebase.initializeApp({
+  apiKey: "AIzaSyDz8mZoFdWLZs9zRC2xDndRzKQ7sju-Goc",
+  authDomain: "eduku-web.firebaseapp.com",
+  projectId: "eduku-web",
+  storageBucket: "eduku-web.firebasestorage.com",
+  messagingSenderId: "662463693471",
+  appId: "1:662463693471:web:e0f19e4497aa3f1de498aa",
+  measurementId: "G-G0VWNHHVB8"
 });
 
-self.addEventListener('push', function(event) {
-  if (event.data) {
-    const data = event.data.json();
-    const options = {
-      body: data.body,
-      icon: data.icon || 'https://c.termai.cc/i46/b87.png',
-      badge: 'https://c.termai.cc/i46/b87.png',
-      data: {
-        url: data.click_action || '/' // URL tujuan saat diklik
-      }
-    };
-    event.waitUntil(
-      self.registration.showNotification(data.title, options)
-    );
-  }
-});
+const messaging = firebase.messaging();
 
-self.addEventListener('notificationclick', function(event) {
-  event.notification.close();
-  event.waitUntil(
-    clients.openWindow(event.notification.data.url)
-  );
+// Ini yang menangani pesan saat Web Tertutup (Background)
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: 'https://c.termai.cc/i46/b87.png', // Ganti icon app kamu
+    badge: 'https://c.termai.cc/i46/b87.png'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
