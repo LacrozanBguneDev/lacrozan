1169dcc9d9d54f9e27c7af0e4682dd7aef97a7a6
LacrozanBguneDev <lacrozanbgune@gmail.com>
2026-01-08 15:53:43 +0700

    Create secretConfig.js

---
+++ api/secretConfig.js
@@ -0,0 +1,10 @@
+// backend/secretConfig.js
+export const secretFirebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY, // simpen di .env
+  authDomain: "eduku-web.firebaseapp.com",
+  projectId: "eduku-web",
+  storageBucket: "eduku-web.firebasestorage.com",
+  messagingSenderId: "662463693471",
+  appId: "1:662463693471:web:e0f19e4497aa3f1de498aa",
+  measurementId: "G-G0VWNHHVB8",
+};
