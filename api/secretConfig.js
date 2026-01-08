a13fce09b0709d38054b5126af5aa181217e0c7b
LacrozanBguneDev <lacrozanbgune@gmail.com>
2026-01-08 16:18:42 +0700

    Update secretConfig.js

--- api/secretConfig.js
+++ api/secretConfig.js
@@ -1,10 +1,15 @@
-// backend/secretConfig.js
-export const secretFirebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY, // simpen di .env
-  authDomain: "eduku-web.firebaseapp.com",
-  projectId: "eduku-web",
-  storageBucket: "eduku-web.firebasestorage.com",
-  messagingSenderId: "662463693471",
-  appId: "1:662463693471:web:e0f19e4497aa3f1de498aa",
-  measurementId: "G-G0VWNHHVB8",
-};
+export default function handler(req, res) {
+  try {
+    res.status(200).json({
+      apiKey: "AIzaSyDz8mZoFdWLZs9zRC2xDndRzKQ7sju-Goc",
+      authDomain: "eduku-web.firebaseapp.com",
+      projectId: "eduku-web",
+      storageBucket: "eduku-web.firebasestorage.com",
+      messagingSenderId: "662463693471",
+      appId: "1:662463693471:web:e0f19e4497aa3f1de498aa",
+      measurementId: "G-G0VWNHHVB8"
+    });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+}
