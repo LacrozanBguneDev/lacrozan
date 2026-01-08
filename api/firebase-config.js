7dacd38b0db8211fbdeda48550e2146ac567bc27
LacrozanBguneDev <lacrozanbgune@gmail.com>
2026-01-08 16:27:26 +0700

    Create firebase-config.js

---
+++ api/firebase-config.js
@@ -0,0 +1,11 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    apiKey: "AIzaSyDz8mZoFdWLZs9zRC2xDndRzKQ7sju-Goc",
+    authDomain: "eduku-web.firebaseapp.com",
+    projectId: "eduku-web",
+    storageBucket: "eduku-web.firebasestorage.com",
+    messagingSenderId: "662463693471",
+    appId: "1:662463693471:web:e0f19e4497aa3f1de498aa",
+    measurementId: "G-G0VWNHHVB8"
+  });
+}
